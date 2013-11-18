b5172dd1b3de2d3d2484188b8e9dd8cbef21753a
BadRenegade <ykdacd123@gmail.com>
2013-11-18 09:48:42 +0800

    Add Macros

--- Header Files/MathEngine.h
+++ Header Files/MathEngine.h
@@ -1,6 +1,7 @@
 #ifndef MATH_ENGINE
 #define MATH_ENGINE
 
+#include <cstring>
 
 ///////Point and Vector///////////////////////////////////////////////
 typedef struct VECTOR2D_TYP
@@ -34,7 +35,7 @@ typedef struct VECTOR4D_TYP
 		float M[4];
 		struct 
 		{
-			x, y, z, w;
+			float x, y, z, w;
 		};
 	};
 }VECTOR4D, POINT4D, *LPVECTOR4D, *LPPOINT4D;
@@ -61,7 +62,7 @@ typedef struct PARAM_LINE3D_TYP
 
 
 ////////Plane//////////////////////////////////////////////////////////////////
-typedef struct PLANE_TYP;
+typedef struct PLANE_TYP
 {
 	POINT3D p0;
 	VECTOR3D v;
@@ -84,7 +85,7 @@ typedef struct MATRIX_4X4_TYP
 			float M30, M31, M32, M33;
 		};
 	};
-}MATRIX_4X4, *PMATRIX_4X4;
+}MATRIX_4X4, *LPMATRIX_4X4;
 
 typedef struct MATRIX_4X3_TYP
 {
@@ -99,7 +100,7 @@ typedef struct MATRIX_4X3_TYP
 			float M30, M31, M32;
 		};
 	};
-}MATRIX_4X3, *PMATRIX_4X3;
+}MATRIX_4X3, *LPMATRIX_4X3;
 
 typedef struct MATRIX_3X3_TYP
 {
@@ -113,7 +114,7 @@ typedef struct MATRIX_3X3_TYP
 			float M20, M21, M22;
 		};
 	};
-}MATRIX_3X3, *PMATRIX_3X3;
+}MATRIX_3X3, *LPMATRIX_3X3;
 
 typedef struct MATRIX_3X2_TYP
 {
@@ -127,7 +128,7 @@ typedef struct MATRIX_3X2_TYP
 			float M20, M21;
 		};
 	};
-}MATRIX_3X2, *PMATRIX_3X2;
+}MATRIX_3X2, *LPMATRIX_3X2;
 
 typedef struct MATRIX_2X2_TYP
 {
@@ -140,7 +141,7 @@ typedef struct MATRIX_2X2_TYP
 			float M10, M11;
 		};
 	};
-}MATRIX_2X2, *PMATRIX_2X2;
+}MATRIX_2X2, *LPMATRIX_2X2;
 
 typedef struct MATRIX_1X2_TYP
 {
@@ -152,7 +153,7 @@ typedef struct MATRIX_1X2_TYP
 			float M00, M01;
 		};
 	};
-}MATRIX_1X2, *PMATRIX_1X2;
+}MATRIX_1X2, *LPMATRIX_1X2;
 
 typedef struct MATRIX_1X3_TYP
 {
@@ -164,7 +165,7 @@ typedef struct MATRIX_1X3_TYP
 			float M00, M01, M02;
 		};
 	};
-}MATRIX_1X3, *PMATRIX_1X3;
+}MATRIX_1X3, *LPMATRIX_1X3;
 
 typedef struct MATRIX_1X4_TYP
 {
@@ -176,7 +177,7 @@ typedef struct MATRIX_1X4_TYP
 			float M00, M01, M02, M03;
 		};
 	};
-}MATRIX_1X4, *PMATRIX_1X4;
+}MATRIX_1X4, *LPMATRIX_1X4;
 
 
 
@@ -196,7 +197,29 @@ typedef struct QUAT_TYP
 			float w, x, y, z;
 		};
 	};
-}QUAT, *PQUAT;
+}QUAT, *LPQUAT;
+
+/////2D polar coordinates//////////////////
+typedef struct POLAR2D_TYP
+{
+	float r;
+	float theta;
+}POLAR2D, *LPPOLAR2D;
+
+//3D cylindrical coordinates//////////////////////////
+typedef struct CYLINDRICAL_TYP
+{
+	float r;
+	float theta;
+	float z;
+}CYLINDRICAL, *PCYLINDRICAL;
+
+typedef struct SPHERICAL3D_TYP
+{
+	float p;
+	float theta;      //the 
+	float phi;
+}SPHERICAL3D, *LPSPHERICAL3D;
 
 
 
@@ -204,7 +227,104 @@ typedef struct QUAT_TYP
 
 
 
+////Function about Vector//////////////////////////////////////
+inline void VECTOR2D_ZERO(LPVECTOR2D pV) { pV->x = pV->y = 0.0; }
+inline void VECTOR3D_ZERO(LPVECTOR3D pV) { pV->x = pV->y = pV->z = 0.0; }
+inline void VECTOR4D_ZERO(LPVECTOR4D pV) { pV->x = pV->y = pV->z = 0.0; pV->w = 1.0; }
+inline void VECTOR2D_INITXY(LPVECTOR2D pV, float x, float y) { pV->x = x; pV->y = y; }
+inline void VECTOR3D_INITXYZ(LPVECTOR3D pV, float x, float y, float z) { pV->x = x; pV->y = y; pV->z = z; }
+inline void VECTOR4D_INITXYZ(LPVECTOR4D pV, float x, float y, float z) { pV->x = x; pV->y = y; pV->z = z; pV->w = 1.0; }
+inline void VECTOR2D_INIT(LPVECTOR2D pDest, LPVECTOR2D pSour) { pDest->x = pSour->x; pDest->y = pSour->y; }
+inline void VECTOR3D_INIT(LPVECTOR3D pDest, LPVECTOR3D pSour) { pDest->x = pSour->x; pDest->y = pSour->y; pDest->z = pSour->z; }
+inline void VECTOR4D_INIT(LPVECTOR4D pDest, LPVECTOR4D pSour) { pDest->x = pSour->x; pDest->y = pSour->y; pDest->z = pSour->z; pDest->w = pSour->w; }
+inline void VECTOR2D_COPY(LPVECTOR2D pDest, LPVECTOR2D pSour) { pDest->x = pSour->x; pDest->y = pSour->y; }
+inline void VECTOR3D_COPY(LPVECTOR3D pDest, LPVECTOR3D pSour) { pDest->x = pSour->x; pDest->y = pSour->y; pDest->z = pSour->z; }
+inline void VECTOR4D_COPY(LPVECTOR4D pDest, LPVECTOR4D pSour) { pDest->x = pSour->x; pDest->y = pSour->y; pDest->z = pSour->z; pDest->w = pSour->w; }
 
 
+
+////Function about Point
+inline void POINT2D_INIT(LPPOINT2D pDest, LPPOINT2D pSour) { pDest->x = pSour->x; pDest->y = pSour->y; }
+inline void POINT3D_INIT(LPPOINT3D pDest, LPPOINT3D pSour) { pDest->x = pSour->x; pDest->y = pSour->y; pDest->z = pSour->z; }
+inline void POINT4D_INIT(LPPOINT4D pDest, LPPOINT4D pSour) { pDest->x = pSour->x; pDest->y = pSour->y; pDest->z = pSour->z; pDest->w = pSour->w; }
+inline void POINT2D_COPY(LPPOINT2D pDest, LPPOINT2D pSour) { pDest->x = pSour->x; pDest->y = pSour->y; }
+inline void POINT3D_COPY(LPPOINT3D pDest, LPPOINT3D pSour) { pDest->x = pSour->x; pDest->y = pSour->y; pDest->z = pSour->z; }
+inline void POINT4D_COPY(LPPOINT4D pDest, LPPOINT4D pSour) { pDest->x = pSour->x; pDest->y = pSour->y; pDest->z = pSour->z; pDest->w = pSour->w; }
+
+
+
+//Const about Matrix
+const MATRIX_4X4 IMAT_4X4 = {1, 0, 0, 0,
+	                         0, 1, 0, 0,
+					         0, 0, 1, 0,
+					         0, 0, 0, 1};
+const MATRIX_4X3 IMAT_4X3 = {1, 0, 0,
+	                         0, 1, 0,
+					         0, 0, 1,
+					         0, 0, 0,};
+const MATRIX_3X3 IMAT_3X3 = {1, 0, 0,
+	                         0, 1, 0,
+					         0, 0, 1};
+const MATRIX_2X2 IMAT_2X2 = {1, 0,
+	                         0, 1};
+///////////////Macro and Function about Matrix//////////////////////////////////////////
+#define MATRIX_ZERO_2X2(m) { memset((void*)m, 0, sizeof(MATRIX_2X2)); }
+#define MATRIX_ZERO_3X3(m) { memset((void*)m, 0, sizeof(MATRIX_3X3)); }
+#define MATRIX_ZERO_4X4(m) { memset((void*)m, 0, sizeof(MATRIX_4X4)); }
+#define MATRIX_ZERO_4X3(m) { memset((void*)m, 0, sizeof(MATRIX_4X3)); }
+
+#define MATRIX_ZERO_2X2(m) { memcpy((void*)m, (void*)&IMAT_2X2, sizeof(MATRIX_2X2)); }
+#define MATRIX_ZERO_3X3(m) { memcpy((void*)m, (void*)&IMAT_3X3, sizeof(MATRIX_3X3)); }
+#define MATRIX_ZERO_4X4(m) { memcpy((void*)m, (void*)&IMAT_4X4, sizeof(MATRIX_4X4)); }
+#define MATRIX_ZERO_4X3(m) { memcpy((void*)m, (void*)&IMAT_4X3, sizeof(MATRIX_4X3)); }
+
+#define MATRIX_COPY_2X2(mDest, mSour) { memcpy((void*)mDest, (void*)mSour, sizeof(MATRIX_2X2)); }
+#define MATRIX_COPY_3X3(mDest, mSour) { memcpy((void*)mDest, (void*)mSour, sizeof(MATRIX_3X3)); }
+#define MATRIX_COPY_4X4(mDest, mSour) { memcpy((void*)mDest, (void*)mSour, sizeof(MATRIX_4X4)); }
+#define MATRIX_COPY_4X3(mDest, mSour) { memcpy((void*)mDest, (void*)mSour, sizeof(MATRIX_4X3)); }
+
+inline void MATRIX_TRANSPOSE_3X3(LPMATRIX_3X3 pM)
+{
+	MATRIX_3X3 m;
+	m.M00 = pM->M00; m.M01 = pM->M10; m.M02 = pM->M20;
+	m.M10 = pM->M01; m.M11 = pM->M11; m.M12 = pM->M21;
+	m.M20 = pM->M02; m.M21 = pM->M12; m.M22 = pM->M22;
+	memcpy((void*)pM, (void*)&m, sizeof(MATRIX_3X3));
+}
+inline void MATRIX_TRANSPOSE_3X3(LPMATRIX_3X3 pDest, LPMATRIX_3X3 pSour)
+{
+	pDest->M00 = pSour->M00; pDest->M01 = pSour->M10; pDest->M02 = pSour->M20;
+	pDest->M10 = pSour->M01; pDest->M11 = pSour->M11; pDest->M12 = pSour->M21;
+	pDest->M20 = pSour->M02; pDest->M21 = pSour->M12; pDest->M22 = pSour->M22;
+}
+
+inline void MATRIX_TRANSPOSE_4X4(LPMATRIX_4X4 pM)
+{
+	MATRIX_4X4 m;
+	m.M00 = pM->M00; m.M01 = pM->M10; m.M02 = pM->M20; m.M03 = pM->M30;
+	m.M10 = pM->M01; m.M11 = pM->M11; m.M12 = pM->M21; m.M13 = pM->M31;
+	m.M20 = pM->M02; m.M21 = pM->M12; m.M22 = pM->M22; m.M23 = pM->M32;
+	m.M30 = pM->M03; m.M31 = pM->M13; m.M32 = pM->M23; m.M33 = pM->M33;
+	memcpy((void*)pM, (void*)&m, sizeof(MATRIX_4X4));
+}
+inline void MATRIX_TRANSPOSE_4X4(LPMATRIX_4X4 pDest, LPMATRIX_4X4 pSour)
+{
+	pDest->M00 = pSour->M00; pDest->M01 = pSour->M10; pDest->M02 = pSour->M20; pDest->M03 = pSour->M30;
+	pDest->M10 = pSour->M01; pDest->M11 = pSour->M11; pDest->M12 = pSour->M21; pDest->M13 = pSour->M31;
+	pDest->M20 = pSour->M02; pDest->M21 = pSour->M12; pDest->M22 = pSour->M22; pDest->M23 = pSour->M32;
+	pDest->M30 = pSour->M03; pDest->M31 = pSour->M13; pDest->M32 = pSour->M23; pDest->M33 = pSour->M33;
+}
+
+
+
+
+
+
+/////  Function about Quaternion
+inline void QUAT_ZERO(LPQUAT pQ) { pQ->x = pQ->y = pQ->z = pQ->w = 0.0; }
+inline void QUAT_INITWXYZ(LPQUAT pQ, float x, float y, float, float z, float w) { pQ->x = x; pQ->y = y; pQ->z = z; pQ->w = w; }
+inline void QUAT_INIT_VECTOR3D(LPQUAT pQ, LPVECTOR3D pV) { pQ->w = 0; pQ->x = pV->x; pQ->y = pV->y; pQ->z = pV->z; }
+inline void QUAT_INIT(LPQUAT pDest, LPQUAT pSour) { pDest->x = pSour->x; pDest->y = pSour->y; pDest->z = pSour->z; pDest->w = pSour->w; }
+inline void QUAT_COPY(LPQUAT pDest, LPQUAT pSour) { pDest->x = pSour->x; pDest->y = pSour->y; pDest->z = pSour->z; pDest->w = pSour->w; }
 
 #endif
